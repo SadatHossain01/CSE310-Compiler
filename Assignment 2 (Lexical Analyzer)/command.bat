06e251052363b9b5a9673bc555e60273f9a80ec8
Mohammad Sadat Hossain <sadatho2014@gmail.com>
2022-12-16 09:10:00 +0600

    updated TOO_DECIMAL regex

--- Assignment 2 (Lexical Analyzer)/command.bat
+++ Assignment 2 (Lexical Analyzer)/command.bat
@@ -1,3 +1,3 @@
-flex scanner.l
+flex 1905001_scanner.l
 g++ lex.yy.c -o a.exe
 a.exe in.txt
--- Assignment 2 (Lexical Analyzer)/command.sh
+++ Assignment 2 (Lexical Analyzer)/command.sh
@@ -1,3 +1,3 @@
-flex -o scanner.c 1905001_scanner.l
-g++ scanner.c -lfl -o scanner.out
+flex -o 1905001_scanner.c 1905001_scanner.l
+g++ 1905001_scanner.c -lfl -o scanner.out
 ./scanner.out in.txt
