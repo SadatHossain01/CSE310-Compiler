e0c35c1822a8a4440e0cd48a05161ccd78757969
SadatHossain01 <sadatho2014@gmail.com>
2023-01-21 01:09:00 +0600

    changed script to also show line number

--- Syntax and Semantic Analyzer/script.sh
+++ Syntax and Semantic Analyzer/script.sh
@@ -1,6 +1,6 @@
 yacc -d parser.y
 flex scanner.l
 # g++ lex.yy.c y.tab.c utilities.cpp -o out
-g++ lex.yy.c y.tab.c utilities.cpp -fsanitize=address -o out
+g++ lex.yy.c y.tab.c utilities.cpp -fsanitize=address -g -o out
 # g++ lex.yy.c y.tab.c utilities.cpp -o out
 ./out errorrecover.c
