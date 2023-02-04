5a5f1838f8aa475dbd787903e06037b434b10df7
SadatHossain01 <sadatho2014@gmail.com>
2023-02-04 21:52:32 +0600

    updated script

--- Intermediate Code Generation/script.sh
+++ Intermediate Code Generation/script.sh
@@ -5,4 +5,4 @@ echo 'Generated the scanner C file'
 g++ lex.yy.c y.tab.c utilities.cpp icg_util.cpp -fsanitize=address -g -o out
 echo 'All ready, running'
 ./out $1
-rm lex.yy.c y.tab.c y.tab.h
+rm lex.yy.c y.tab.c y.tab.h error.txt log.txt
