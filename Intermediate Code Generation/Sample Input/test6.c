a3171f01348fc18212217249fe1cf56371ce154d
Sadat Hossain <sadatho2014@gmail.com>
2023-02-20 21:00:44 +0600

    hopefully final commit, added some test cases

---
+++ Intermediate Code Generation/Sample Input/test6.c
@@ -0,0 +1,19 @@
+int j;
+int func(int a, int b) {
+    int j;
+    j = 45;
+    return (a <= b) + (a && b);
+    println(j);
+    j = 3;
+}
+
+int main() {
+    int a, b;
+    j = 69;
+    a = 2;
+    b = 3;
+    int k;
+    k = func(a, b);
+    println(k);
+    println(j);
+}
